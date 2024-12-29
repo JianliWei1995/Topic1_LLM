c0b3f1c36035984019f97282890a72ada368978e
JianliWei1995 <wei.909@osu.edu>
2024-12-28 22:27:03 -0500

    updated

--- Week3/minbpe/minbpe/base.py
+++ Week3/minbpe/minbpe/base.py
@@ -1,2 +1,67 @@
 import unicodedata
 
+# some helper functions
+
+def get_stats(ids, counts=None):
+    """
+    Given a list of integers, return a dictionary of counts of consecutive pairs
+    Example: [1, 2, 3, 1, 2] -> {(1, 2): 2, (2, 3): 1, (3, 1): 1}
+    Optionally allows to update an existing dictionary of counts
+    """
+    counts = {} if counts is None else counts
+    for pair in zip(ids, ids[1:]):
+        counts[pair] = counts.get(pair, 0) + 1
+    return counts
+
+def merge(ids, pair, idx):
+    """
+    In the list of integers (ids), replace all consecutive occurrences
+    of pair with the new integer token idx
+    Example: ids=[1, 2, 3, 1, 2], pair=(1, 2), idx=4 -> [4, 3, 4]
+    """
+    newids = []
+    i = 0
+    while i < len(ids):
+        # if not at the very last position AND the pair matches, replace it
+        while i < len(ids):
+            if ids[i]==pair[0] and i < len(ids) - 1 and ids[i+1]==pair[1]:
+                newids.append(idx)
+                i +=2
+            else:
+                newids.append(ids[i])
+                i += 1
+    return newids
+
+#  ---------------------------------------
+# the base Tokenizer class
+
+class Tokenizer:
+    """Base class for Tokenizers"""
+
+    def __init__(self) -> None:
+        # default: vocab size of 256 (all bytes), no merges, no spaces
+        self.merges = {} # (int, int) -> int
+        self.pattern = "" # str
+        self.spacial_tokens = {} # str, e.g. {'<|endoftext|>': 10257}
+        self.vocab = self._build_vocab() # int -> bytes
+
+    def train(self, text, vocab_size, verbose=False):
+        # Tokenizer can train a vocabulary of size vocab_size from text
+        raise NotImplementedError
+
+    def encode(self, text):
+        # Tokenizer can encode a string to a list of integers
+        raise NotImplementedError
+    
+    def decode(self, ids):
+        # Tokenizer can decode a list of integers to a string
+        raise NotImplementedError
+    
+    def _build_vocab(self):
+        # vocab is simply and deterministically derived from merges
+        vocab = {idx: bytes([idx]) for idx in range(256)}
+        for (p0, p1), idx in self.merges.items():
+            vocab[idx] = vocab[p0] + vocab[p1]
+        for special, idx in self.spacial_tokens.items():
+            vocab[idx] = special.encode("utf-8")
+        return vocab
--- Week3/minbpe/train.py
+++ Week3/minbpe/train.py
@@ -1,1 +1,32 @@
-import torch
+import os
+import time
+
+from minbpe.basic import BasicTokenizer
+
+if __name__=="__main__":
+
+    # cd to current .py directyory
+    target_dir = "./Week3/minbpe"
+    os.chdir(target_dir)
+
+    # create a directory for models, so we don't pollute the current directory
+    os.makedirs("models", exist_ok=True)
+
+    # open some text and train a vocab of 512 tokens
+    text = open("./test/taylorswift.txt", "r", encoding="utf-8").read()
+
+    t0 = time.time()
+
+    tokenizer = BasicTokenizer()
+    tokenizer.train(text, 512, verbose=False)
+    
+    text = "hello world!I love you because I really think you are the best and you are the best gift I can ever get in my life. I hope I can stay with you in my whole life and I will use out of my love and treat you as my little baby"
+    tokens = tokenizer.encode(text)
+    new_text = tokenizer.decode(tokens)
+    print(tokens)
+    print(new_text)
+    print(text==new_text)
+
+    t1 = time.time()
+
+    print(f"Training took {t1 - t0:.2f} seconds")
